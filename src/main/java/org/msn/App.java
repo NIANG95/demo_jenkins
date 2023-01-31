2d22bb91792eed36fa71a133d0906e85bf478413
MAME SANDECK NIANG <63318185+NIANG95@users.noreply.github.com>
2023-01-31 15:44:37 +0000

    Update App.java

--- src/main/java/org/msn/App.java
+++ src/main/java/org/msn/App.java
@@ -4,6 +4,6 @@ public class App {
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from Mame Sandeck Niang !" );
+        System.out.println( "Hello World !" );
     }
 }
